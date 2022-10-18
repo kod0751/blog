9ca6573d034de398387f913e782b6bab4a786bc8
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-10-18 17:19:24 +0900

    Dynamic Routes, remark, remark-html
    getAllPosts - 포스트 이름을 모두 가져오는 함수
    
    getPostData - id를 가지고 포스트의 내용을 가져오는 함수
    
    remark, remark-html은 markdown을 html로 치환해주는 역할을 해줌

---
+++ pages/posts/[id].js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function FirstPost({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
