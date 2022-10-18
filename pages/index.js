10bb62646e9fa92ae48a5df6e1d8100c3f6faa72
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-10-18 14:48:38 +0900

    SSG

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,23 @@ import utilStyles from "../styles/utils.module.css";
 //   };
 // }
 
-export default function Home() {
-  const [allPostsData, setAllPostsData] = useState([]);
-  useEffect(() => {
-    fetch("/api/posts")
-      .then((res) => res.json())
-      .then((data) => setAllPostsData(data.allPostsData));
-  }, []);
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3000/api/posts");
+  const json = await response.json();
+  return {
+    props: {
+      allPostsData: json.allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  // const [allPostsData, setAllPostsData] = useState([]);
+  // useEffect(() => {
+  //   fetch("/api/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => setAllPostsData(data.allPostsData));
+  // }, []);
   return (
     <Layout home>
       <Head>
