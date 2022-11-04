b4c521e3a1f8eaaaddd782c058d983416f2096f8
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-11-04 20:51:40 +0900

    MDX 사용해보기

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,9 @@ import Layout from "../../components/Layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/Date";
 import utilStyles from "../../styles/utils.module.css";
+import { MDXRemote } from "next-mdx-remote";
+import { useRouter } from "next/router";
+import CodeBlock from "../../components/CodeBlock";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -21,14 +24,37 @@ export async function getStaticProps({ params }) {
   };
 }
 
+const Button = ({ children }) => {
+  return (
+    <button
+      className="bg-black dark:bg-white text-lg text-teal-200 dark:text-teal-700 rounded-lg px-5"
+      onClick={() => alert(`thanks to ${children}`)}
+    >
+      {children}
+    </button>
+  );
+};
+
+const components = { Button, CodeBlock };
+
 export default function FirstPost({ postData }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
   return (
     <Layout>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}></div>
         <Date dateString={postData.date} />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        {postData.contentHtml && (
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        )}
+        {postData.mdxSource && (
+          <MDXRemote {...postData.mdxSource} components={components} />
+        )}
       </article>
     </Layout>
   );
