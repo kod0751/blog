aa554d2d13009eb26a996069111dc73fb8c34765
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-11-10 17:03:18 +0900

    Preview Mdoe
    쿠키를 이용해서 getStaticProps를 request time에도 실행하도록 만들어준다

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -22,7 +22,8 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params, preview }) {
+  console.log(`>>>>>> ${preview}`);
   const postData = await getPostData(params.id);
   return {
     props: {
